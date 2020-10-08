b4d061cdcbe60eb0828acc0d4dc326fdc8bed306
sebaf96 <sefernandez96@gmail.com>
2020-10-08 17:37:24 -0300

    Add basic header and moved alerts to app layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'subjects#index'
   resources :subjects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
