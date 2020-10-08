4c0f7bea33065f61559554cd71d6392c231d9d64
sebaf96 <sefernandez96@gmail.com>
2020-10-08 18:21:35 -0300

    Improve header and flash messages

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,13 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        var $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+});
