4a4ea06e3201edc15fd57cc519b1a1dd6e6219e4
sebaf96 <sefernandez96@gmail.com>
2020-10-09 18:12:21 -0300

    finish signup form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,14 @@ class User < ApplicationRecord
   has_many :subjects, through: :exams
 
   # Name validations
-  validates :name, presence: { message: "Ingresa tu nombre" }
   validates :name, format: { with: /\A[a-zA-Z]+\z/, message: 'El nombre solo debe contener letras' }
 
   # Email validations
   validates :email, format: {with: /[\w]+@alumno.um.edu.ar/, message: "El email debe terminar en @alumno.um.edu.ar"}
-  validates :email, presence: { message: "Ingresa tu email" }
   validates :email, uniqueness: { message: "Este correo ya esta registrado" }
 
   # Password validations
-  # validates :password, presence: {message: "Ingresa tu contraseña"}
-  validates :password, confirmation: {message: "Las contraseñas no coinciden"}
+  validates_confirmation_of :password
 
 
 end
