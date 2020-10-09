90439a53d62227606dc14c9ab16842d863a8e947
sebaf96 <sefernandez96@gmail.com>
2020-10-09 02:39:55 -0300

    sort subjects by code

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,7 +4,7 @@ class SubjectsController < ApplicationController
   # GET /subjects
   # GET /subjects.json
   def index
-    @subjects = Subject.all
+      @subjects = Subject.all.sort {|a, b| a.code <=> b.code}
   end
 
   # GET /subjects/1
