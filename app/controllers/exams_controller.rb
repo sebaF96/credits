5c300a7558c32913dc3f26414b320f0cd316a4d0
sebaf96 <sefernandez96@gmail.com>
2020-10-09 23:16:13 -0300

    add logic to approve exam

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,2 +1,23 @@
 class ExamsController < ApplicationController
+  before_action :require_user
+
+  def index
+    @subjects = current_user.not_passed
+  end
+  
+  def new
+    @subject = Subject.find_by_code(params[:code])
+    @user = current_user
+  end
+
+  def create
+
+  end
+
+  private
+  # Only allow a list of trusted parameters through.
+  def exam_params
+    params.require(:subject).permit(:user, :subject, :qualification)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   resources :subjects
   resources :users, only: [:index, :new, :create, :show]
+  resources :exams, except: [:new]
+
+  get "exams/new/:code", to: "exams#new", as: "new_exam"
 
   # Sessions
   get "signup", to: "users#new", as: "signup"
