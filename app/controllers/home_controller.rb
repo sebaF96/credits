bfbbf35fb1c9efc914bfd1c15239c8582a24043e
sebaf96 <sefernandez96@gmail.com>
2020-10-10 20:05:49 -0300

    finish half index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,11 @@ class HomeController < ApplicationController
   before_action :require_user
 
   def index
-    @subjects = current_user.exams
+    @passed = current_user.passed
+    @not_passed = current_user.not_passed
+    @percent = (@passed.size.to_f / (@passed.size.to_f + @not_passed.size.to_f) * 100).truncate(2)
+    @credits = 0
+    @passed.each {|s| @credits += s.credits}
   end
 
 end
