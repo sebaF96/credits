4e31ede6f49654ede76e4f67196dbbf966b4a023
sebaf96 <sefernandez96@gmail.com>
2020-10-11 00:50:01 -0300

    complete seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,66 @@
 
 # PRIMERO
 
-Subject.create(code: "0001", name: "Tecnologia Ciencia y Sociedad", credits: 3, year: 1)
+Subject.create(code: "0001", name: "Tecn. Ciencia y Sociedad", credits: 3, year: 1)
 Subject.create(code: "0002", name: "Quimica General", credits: 4, year: 1)
 Subject.create(code: "0003", name: "Calculo I", credits: 3, year: 1)
 Subject.create(code: "0004", name: "Calculo II", credits: 3, year: 1)
 Subject.create(code: "0005", name: "Sistemas de Representacion", credits: 3, year: 1)
 Subject.create(code: "0006", name: "Fisica I", credits: 7, year: 1)
-Subject.create(code: "0007", name: "Algebra y Geom. Analitica")
+Subject.create(code: "0007", name: "Algebra y Geom. Analitica", credits: 5, year: 1)
 Subject.create(code: "0008", name: "Ingles Tecnico", credits: 3, year: 1)
 Subject.create(code: "0009", name: "Informatica", credits: 7, year: 1)
 
-# User.create(name: "Seba", email: "sebaf@mm.com")
+
+# SEGUNDO
+
+Subject.create(code: "2020", name: "Fisica II", credits: 7, year: 2)
+Subject.create(code: "2021", name: "Calculo III", credits: 3, year: 2)
+Subject.create(code: "2022", name: "Mat. Discreta", credits: 3, year: 2)
+Subject.create(code: "2023", name: "Arq. de computadoras", credits: 3, year: 2)
+Subject.create(code: "2024", name: "Estadistica I", credits: 4, year: 2)
+Subject.create(code: "2025", name: "Analisis de Sistemas I", credits: 5, year: 2)
+Subject.create(code: "2026", name: "Sistemas Operativos", credits: 4, year: 2)
+Subject.create(code: "2027", name: "Algebra Lineal", credits: 3, year: 2)
+Subject.create(code: "2028", name: "Computacion I", credits: 6, year: 2)
+
+# TERCERO
+
+Subject.create(code: "2030", name: "Analisis Numerico", credits: 5, year: 3)
+Subject.create(code: "2031", name: "Analisis de Sistemas II", credits: 4, year: 3)
+Subject.create(code: "2032", name: "Redes de Datos", credits: 6, year: 3)
+Subject.create(code: "2033", name: "Estadistica II", credits: 3, year: 3)
+Subject.create(code: "2034", name: "Programacion I", credits: 4, year: 3)
+Subject.create(code: "2035", name: "Teoria de Compiladores", credits: 3, year: 3)
+Subject.create(code: "2036", name: "Diseño de Sistemas", credits: 5, year: 3)
+Subject.create(code: "2037", name: "Comunicacion de Datos", credits: 3, year: 3)
+Subject.create(code: "2038", name: "Computacion II", credits: 5, year: 3)
+
+# CUARTO
+
+Subject.create(code: "2040", name: "Diseño de BD I", credits: 5, year: 4)
+Subject.create(code: "2041", name: "Ing. de software", credits: 5, year: 4)
+Subject.create(code: "2042", name: "Teleinformatica", credits: 5, year: 4)
+Subject.create(code: "2043", name: "Organizacion", credits: 3, year: 4)
+Subject.create(code: "2044", name: "Seguridad Informatica", credits: 5, year: 4)
+Subject.create(code: "2045", name: "Auditoria de Sistemas I", credits: 4, year: 4)
+Subject.create(code: "2046", name: "Economia", credits: 3, year: 4)
+Subject.create(code: "2047", name: "Investigacion Operativa", credits: 4, year: 4)
+Subject.create(code: "2048", name: "Form. y Evaluacion de Proyectos", credits: 4, year: 4)
+Subject.create(code: "2049", name: "Programacion II", credits: 5, year: 4)
+Subject.create(code: "3000", name: "Optativa I", credits: 3, year: 4)
+
+
+# QUINTO
+
+Subject.create(code: "2050", name: "Ing. Derecho y Etica Profesional", credits: 3, year: 5)
+Subject.create(code: "2051", name: "Gestion y calidad de medio ambiente", credits: 4, year: 5)
+Subject.create(code: "2052", name: "Higiene y seguridad del trabajo", credits: 3, year: 5)
+Subject.create(code: "2053", name: "Modelos y simulacion", credits: 3, year: 5)
+Subject.create(code: "2054", name: "Diseño de BD II", credits: 5, year: 5)
+Subject.create(code: "2055", name: "Inteligencia Artificial", credits: 4, year: 5)
+Subject.create(code: "2056", name: "Planeamiento y gestion de empresas", credits: 5, year: 3)
+Subject.create(code: "2057", name: "Trabajo Final", credits: 7, year: 5)
+Subject.create(code: "2058", name: "Practica Profesional", credits: 0, year: 5)
+Subject.create(code: "4000", name: "Optativa II", credits: 3, year: 5)
+Subject.create(code: "5000", name: "Optativa III", credits: 3, year: 5)
