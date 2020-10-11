dbbd4943bdff3a8f1ec1fc57b52f2fd5d58445be
sebaf96 <sefernandez96@gmail.com>
2020-10-11 19:00:37 -0300

    add avg to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,11 @@ class HomeController < ApplicationController
     @not_passed = current_user.not_passed
     @percent = (@passed.size.to_f / (@passed.size.to_f + @not_passed.size.to_f) * 100).truncate(2)
     @credits = 0
-    @passed.each {|s| @credits += s.credits}
+    @passed.each { |s| @credits += s.credits }
+    total_notes = (current_user.passed.map { |s| current_user.qualification_on(s) }).sum.to_f
+    @avg = 0.to_f.truncate(1)
+    @avg = (total_notes / @passed.size).to_f.truncate(1) unless @passed.size == 0
+
   end
 
 end
