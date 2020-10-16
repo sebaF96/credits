229ce4ae1f39abcc8a5340c08f2703e3acfe6080
sebaf96 <sefernandez96@gmail.com>
2020-10-16 00:58:50 -0300

    add production config for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,18 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ruby_credits_production"
 
-  config.action_mailer.perform_caching = false
+
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'https://umcredits.herokuapp.com/', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'https://umcredits.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'gmail.com',
-      user_name: ENV["MAILER_ADDRESS"],
-      password: ENV["MAILER_PASSWORD"],
-      authentication: 'plain'
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'umcredits.herokuapp.com',
+    user_name: ENV['MAILER_ADDRESS'],
+    password: ENV['MAILER_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -93,7 +94,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
